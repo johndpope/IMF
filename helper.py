a7fc004a2e75c47df37d7b4ef3c4ca0592718a97
John Pope <jp@bellgeorge.com>
2024-08-11 05:49:23 +1000

    🧗

--- helper.py
+++ helper.py
@@ -265,13 +265,13 @@ def count_model_params(model, trainable_only=False, verbose=False):
             print(f"Native {i}. {layer_type[7:]:<23} {count:<15,d} {percentage:.2f}%")
         
         print("-" * 55)
-        print(f"{'Native Modules Total':<30} {native_total:<15,d} {native_total/total_params*100:.2f}%")
+        print(f"{'☕ Native Modules Total':<30} {native_total:<15,d} {native_total/total_params*100:.2f}%")
         print("-" * 55)
         
         # Print custom modules
         for i, (layer_type, count) in enumerate(sorted(custom_counts.items(), key=lambda x: x[1], reverse=True), 1):
             percentage = count / total_params * 100
-            print(f"Custom {i}. {layer_type[7:]:<23} {count:<15,d} {percentage:.2f}%")
+            print(f"🍄 Custom {i}. {layer_type[7:]:<23} {count:<15,d} {percentage:.2f}%")
         
         print("-" * 55)
         print(f"{'Custom Modules Total':<30} {custom_total:<15,d} {custom_total/total_params*100:.2f}%")
