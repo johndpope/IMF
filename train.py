9a6d468c7fd4d8fe63c8d59943911e8bc28c9278
John Pope <jp@bellgeorge.com>
2024-08-13 09:42:07 +1000

    🧗

--- train.py
+++ train.py
@@ -110,7 +110,7 @@ def train(config, model, discriminator, train_dataloader, val_loader, accelerato
 
     # Use the unified gan_loss_fn
     gan_loss_type = config.loss.type
-    perceptual_loss_fn = lpips.LPIPS(net='alex').to(accelerator.device)
+    perceptual_loss_fn = lpips.LPIPS(net='alex',spatial=config.loss.lpips_spatial).to(accelerator.device)
     pixel_loss_fn = nn.L1Loss()
     
 
