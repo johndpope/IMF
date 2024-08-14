cbd086b2d348348a29796608cdfc2e03d8f4e5bd
John Pope <jp@bellgeorge.com>
2024-08-14 16:47:14 +1000

    ok

--- VideoDataset.py
+++ VideoDataset.py
@@ -44,30 +44,16 @@ class VideoDataset(Dataset):
         self.transform = transform
         self.frame_skip = frame_skip
         self.num_frames = num_frames
-        self.is_gcs = root_dir.startswith('gs://')
-        if self.is_gcs:
-            self.client = storage.Client()
-            self.bucket_name = root_dir.split('/')[2]
-            self.bucket = self.client.get_bucket(self.bucket_name)
-            self.prefix = '/'.join(root_dir.split('/')[3:])
         self.video_folders = self._list_folders()
         self.video_frames = self._count_frames()
 
     def _list_folders(self):
-        if self.is_gcs:
-            blobs = self.bucket.list_blobs(prefix=self.prefix, delimiter='/')
-            return [f"gs://{self.bucket_name}/{prefix}" for prefix in blobs.prefixes]
-        else:
-            return [f.path for f in os.scandir(self.root_dir) if f.is_dir()]
+        return [f.path for f in os.scandir(self.root_dir) if f.is_dir()]
 
     def _count_frames(self):
         video_frames = []
         for folder in self.video_folders:
-            if self.is_gcs:
-                blobs = self.bucket.list_blobs(prefix=folder.split(f"gs://{self.bucket_name}/")[1])
-                frames = [blob.name for blob in blobs if blob.name.endswith('.jpg')]
-            else:
-                frames = [f for f in os.listdir(folder) if f.endswith('.jpg')]
+            frames = [f for f in os.listdir(folder) if f.endswith('.jpg')]
             video_frames.append(len(frames))
         return video_frames
 
@@ -82,11 +68,7 @@ class VideoDataset(Dataset):
 
     def __getitem__(self, idx):
         video_folder = self.video_folders[idx]
-        if self.is_gcs:
-            blobs = list(self.bucket.list_blobs(prefix=video_folder.split(f"gs://{self.bucket_name}/")[1]))
-            frames = sorted([blob.name for blob in blobs if blob.name.endswith('.jpg')])
-        else:
-            frames = sorted([f for f in os.listdir(video_folder) if f.endswith('.jpg')])
+        frames = sorted([f for f in os.listdir(video_folder) if f.endswith('.jpg')])
         
         if len(frames) < self.num_frames:
             frames = frames + [frames[-1]] * (self.num_frames - len(frames))
@@ -96,13 +78,8 @@ class VideoDataset(Dataset):
         
         vid_pil_image_list = []
         for i in frame_indices:
-            if self.is_gcs:
-                blob = self.bucket.blob(frames[i])
-                img_bytes = blob.download_as_bytes()
-                img = Image.open(BytesIO(img_bytes)).convert('RGB')
-            else:
-                img_path = os.path.join(video_folder, frames[i])
-                img = Image.open(img_path).convert('RGB')
+            img_path = os.path.join(video_folder, frames[i])
+            img = Image.open(img_path).convert('RGB')
             vid_pil_image_list.append(img)
 
         if self.transform:
@@ -116,7 +93,6 @@ class VideoDataset(Dataset):
             "video_name": os.path.basename(video_folder)
         }
 
-
 # Example usage
 if __name__ == "__main__":
     transform = transforms.Compose([
@@ -124,7 +100,8 @@ if __name__ == "__main__":
         transforms.ToTensor(),
     ])
 
-    dataset = VideoDataset("/media/oem/12TB/Downloads/CelebV-HQ/celebvhq/35666/images", 
+    # Assume the GCS bucket is mounted at /mnt/gcs_bucket
+    dataset = VideoDataset("/mnt/gcs_bucket/CelebV-HQ/celebvhq/35666/images", 
                            transform=transform, 
                            frame_skip=0, 
                            num_frames=2)
@@ -132,6 +109,6 @@ if __name__ == "__main__":
 
     sample = dataset[0]
     print(f"Sample keys: {sample.keys()}")
-    print(f"Number of PIL images: {len(sample['frames'])}")
-    print(f"Tensor shape: {sample['tensor'].shape}")
+    print(f"Number of frames: {sample['frames'].shape[0]}")
+    print(f"Tensor shape: {sample['frames'].shape}")
     print(f"Video name: {sample['video_name']}")
