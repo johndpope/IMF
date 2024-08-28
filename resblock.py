5e478242c2c0c5c815d5458b5c666cf373fcc5ea
John Pope <jp@bellgeorge.com>
2024-08-28 17:09:04 +1000

    add in conv2dmod

--- resblock.py
+++ resblock.py
@@ -98,11 +98,51 @@ class ModulatedConv2d(nn.Module):
         out = out.view(batch, self.weight.size(0), height, width)
 
         return out
+    
+
+# https://github.com/lucidrains/stylegan2-pytorch
+class Conv2DMod(nn.Module):
+    def __init__(self, in_chan, out_chan, kernel, demod=True, stride=1, dilation=1, eps = 1e-8, **kwargs):
+        super().__init__()
+        self.filters = out_chan
+        self.demod = demod
+        self.kernel = kernel
+        self.stride = stride
+        self.dilation = dilation
+        self.weight = nn.Parameter(torch.randn((out_chan, in_chan, kernel, kernel)))
+        self.eps = eps
+        nn.init.kaiming_normal_(self.weight, a=0, mode='fan_in', nonlinearity='leaky_relu')
+
+    def _get_same_padding(self, size, kernel, dilation, stride):
+        return ((size - 1) * (stride - 1) + dilation * (kernel - 1)) // 2
+
+    def forward(self, x, y):
+        b, c, h, w = x.shape
+
+        w1 = y[:, None, :, None, None]
+        w2 = self.weight[None, :, :, :, :]
+        weights = w2 * (w1 + 1)
+
+        if self.demod:
+            d = torch.rsqrt((weights ** 2).sum(dim=(2, 3, 4), keepdim=True) + self.eps)
+            weights = weights * d
+
+        x = x.reshape(1, -1, h, w)
+
+        _, _, *ws = weights.shape
+        weights = weights.reshape(b * self.filters, *ws)
+
+        padding = self._get_same_padding(h, self.kernel, self.dilation, self.stride)
+        x = F.conv2d(x, weights, padding=padding, groups=b)
+
+        x = x.reshape(-1, self.filters, h, w)
+        return x
+    
 
 class StyledConv(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size, style_dim, upsample=False, demodulate=True):
         super().__init__()
-        self.conv = ModulatedConv2d(in_channels, out_channels, kernel_size, demodulate=demodulate)
+        self.conv = Conv2DMod(in_channels, out_channels, kernel_size, demod=demodulate)
         self.style = nn.Linear(style_dim, in_channels)
         self.upsample = upsample
         self.activation = nn.LeakyReLU(0.2)
