113ca2c5dde11adfcac960ea9aefd37d66210d12
John Pope <jp@bellgeorge.com>
2024-09-05 09:32:41 +1000

    ok

--- train_token.py
+++ train_token.py
@@ -6,30 +6,82 @@ from accelerate import Accelerator
 from omegaconf import OmegaConf
 import wandb
 from tqdm.auto import tqdm
-from model import TokenManipulationNetwork
+from model import TokenManipulationNetwork,IMFModel
 
 
-class StyleFeatureEditorWrapper(nn.Module):
+# class StyleFeatureEditorWrapper(nn.Module):
+#     def __init__(self, sfe_model, token_manipulation_network):
+#         super().__init__()
+#         self.sfe_model = sfe_model
+#         self.token_manipulation_network = token_manipulation_network
+
+#     def forward(self, x_current, x_reference, editing_condition):
+#         # SFE inversion
+#         w, F_k = self.sfe_model.inverter(x_current)
+        
+#         # Token manipulation
+#         edited_w = self.token_manipulation_network(w, editing_condition)
+        
+#         # SFE editing
+#         delta = self.sfe_model.get_delta(w, edited_w)
+#         F_k_edited = self.sfe_model.feature_editor(F_k, delta)
+        
+#         # SFE reconstruction
+#         x_edited = self.sfe_model.frame_decoder(F_k_edited, edited_w[self.sfe_model.k+1:])
+        
+#         return x_edited, w, edited_w, F_k, F_k_edited
+
+class StyleFeatureEditor(nn.Module):
+    def __init__(self, device="cuda:0", paths=DefaultPaths, checkpoint_path=None, inverter_pth=None):
+        super(StyleFeatureEditor, self).__init__()
+        self.fse_full = FSEFull(device, paths, checkpoint_path, inverter_pth)
+        self.fse_inverter = FSEInverter(device, paths, checkpoint_path)
+
+    def forward(self, x, return_latents=False, n_iter=1e5):
+        # Use FSEFull for editing
+        images, w_recon, fused_feat, predicted_feat = self.fse_full(x, return_latents=True, n_iter=n_iter)
+        
+        # Use FSEInverter for inversion
+        inverted_images, w_recon_inv, fused_feat_inv, w_feat_inv = self.fse_inverter(x, return_latents=True, n_iter=n_iter)
+        
+        if return_latents:
+            return images, w_recon, fused_feat, predicted_feat, inverted_images, w_recon_inv, fused_feat_inv, w_feat_inv
+        return images, inverted_images
+
+    def get_delta(self, w, edited_w):
+        # Compute delta based on the difference between w and edited_w
+        return edited_w - w
+
+class CombinedModel(nn.Module):
     def __init__(self, sfe_model, token_manipulation_network):
         super().__init__()
         self.sfe_model = sfe_model
         self.token_manipulation_network = token_manipulation_network
 
-    def forward(self, x_current, x_reference, editing_condition):
-        # SFE inversion
-        w, F_k = self.sfe_model.inverter(x_current)
+    def forward(self, x, editing_condition):
+        # SFE inversion and editing
+        images, w_recon, fused_feat, predicted_feat, inverted_images, w_recon_inv, fused_feat_inv, w_feat_inv = self.sfe_model(x, return_latents=True)
         
         # Token manipulation
-        edited_w = self.token_manipulation_network(w, editing_condition)
+        edited_w = self.token_manipulation_network(w_recon, editing_condition)
         
-        # SFE editing
-        delta = self.sfe_model.get_delta(w, edited_w)
-        F_k_edited = self.sfe_model.feature_editor(F_k, delta)
+        # SFE editing with manipulated token
+        delta = self.sfe_model.get_delta(w_recon, edited_w)
+        edited_feat = self.sfe_model.fse_full.encoder(torch.cat([fused_feat, delta], dim=1))
+        feats = [None] * 9 + [edited_feat] + [None] * (17 - 9)
         
-        # SFE reconstruction
-        x_edited = self.sfe_model.frame_decoder(F_k_edited, edited_w[self.sfe_model.k+1:])
+        edited_images, _ = self.sfe_model.fse_full.decoder(
+            [edited_w],
+            input_is_latent=True,
+            return_features=True,
+            new_features=feats,
+            feature_scale=1.0,
+            is_stylespace=False,
+            randomize_noise=False
+        )
         
-        return x_edited, w, edited_w, F_k, F_k_edited
+        return edited_images, images, inverted_images, w_recon, edited_w, fused_feat, fused_feat_inv
+
 
 
 def train_token_manipulation_network(config):
@@ -45,23 +97,21 @@ def train_token_manipulation_network(config):
         num_layers=config.model.num_layers,
         use_resnet_feature=config.model.use_resnet_feature
     )
-    imf_checkpoint = torch.load(config.imf_model_path, map_location='cpu')
+    imf_checkpoint = torch.load(config.training.checkpoint_path, map_location='cpu')
     imf_model.load_state_dict(imf_checkpoint['model_state_dict'])
     imf_model.eval()
 
     # Create the TokenManipulationNetwork
+    sfe_model = StyleFeatureEditor(config.device, config.paths, config.checkpoint_path, config.inverter_pth)
     token_manipulation_network = TokenManipulationNetwork(
-        token_dim=config.token_manipulation.token_dim,
-        condition_dim=config.token_manipulation.condition_dim,
-        hidden_dim=config.token_manipulation.hidden_dim
+        token_dim=512,  # Assuming w_recon has 512 dimensions
+        condition_dim=config.condition_dim,
+        hidden_dim=config.hidden_dim
     )
 
-    sfe_model = StyleFeatureEditor(config.sfe)
-    sfe_model.load_state_dict(torch.load(config.sfe_checkpoint_path))
-    sfe_model.eval()  # Set SFE to evaluation mode
 
-
-    combined_model = StyleFeatureEditorWrapper(sfe_model, token_manipulation_network)
+    combined_model = CombinedModel(sfe_model, token_manipulation_network)
+    
 
     # Set up optimizer and dataset
     optimizer = optim.Adam(token_manipulation_network.parameters(), lr=config.token_manipulation.learning_rate)
@@ -81,8 +131,9 @@ def train_token_manipulation_network(config):
         optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
         start_epoch = checkpoint['epoch'] + 1
 
+
     # Training loop
-     for epoch in range(config.token_manipulation.num_epochs):
+    for epoch in range(config.token_manipulation.num_epochs):
         for batch in dataloader:
             x_current, x_reference, editing_condition = batch
             
